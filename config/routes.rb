7174d06d99da4802d78ca9ae39901fdca7f05b68
manasa <mjagadeesh@qwinixtech.com>
2014-03-06 17:41:24 +0530

    integration of home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 CommunityWebsite::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+   get "/:pge" => "pages#show"
+   root :to => "pages#show"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
