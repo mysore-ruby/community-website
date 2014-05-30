943ee247093b684bed050eb29dac4435615b1bbc
Prakash <plingaiah@qwinixtech.com>
2014-05-30 13:08:00 +0530

    integrate google authentication is done

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,6 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, "1489700394593754", "247a3123bf5005cd27ba3190e9403baf"
+  provider :google_oauth2, '817752202648-3c0p0ocd60u8gug9h9q7glmtgv6a69iu.apps.googleusercontent.com', 'J7FFtjPAKFNk20XmW_6-j4I1'
 end
 
